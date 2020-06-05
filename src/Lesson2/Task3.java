27c519e8a0c6a3e28a0fd3fceea50d76dfb15229
Gulnaz Aizatullina <gulnazka.aiza@gmail.com>
2020-06-05 16:49:11 +0300

    Задача выполнена

--- src/Lesson2/Task3.java
+++ src/Lesson2/Task3.java
@@ -4,7 +4,6 @@ public class Task3 {
     public static void main(String[] args) {
         int second = 3600;
         int hours = second / 60;
-
         System.out.println("Колличество секунд: " + second);
         System.out.println("Колличество часов: " + hours);
 
