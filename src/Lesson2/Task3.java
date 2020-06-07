06790990c17457b7c437fc949531c6a5711c5d74
Gulnaz Aizatullina <gulnazka.aiza@gmail.com>
2020-06-07 12:48:50 +0300

    Получилооось!:)

--- src/Lesson2/Task3.java
+++ src/Lesson2/Task3.java
@@ -3,7 +3,7 @@ package Lesson2;
 public class Task3 {
     public static void main(String[] args) {
         int second = 3600;
-        int hours = second / 60;
+        int hours = second / 3600;
         System.out.println("Колличество секунд: " + second);
         System.out.println("Колличество часов: " + hours);
 
